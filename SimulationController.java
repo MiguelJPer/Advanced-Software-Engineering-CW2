496df4633298b0d5f59acf6d48c1b36c29cb58e5
ClarkZhangzy <161899436+ClarkZhangzy@users.noreply.github.com>
2024-03-28 00:20:34 +0000

    Create SimulationController.java

---
+++ SimulationController.java
@@ -0,0 +1,27 @@
+public class SimulationController {
+    public static void main(String[] args) {
+        QueueSystem queueSystem = new QueueSystem();
+        CheckInDesk checkInDesk = new CheckInDesk(queueSystem);
+        Thread deskThread = new Thread(checkInDesk);
+        deskThread.start();
+
+        for (int i = 1; i <=9 ; i++) {
+            Passenger passenger = new Passenger("Passenger" + i, "Flight100", 15.0 + i, 1.0);
+            queueSystem.addPassengerToQueue(passenger);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Thread.sleep(10000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        checkInDesk.setOpen(false); 
+        deskThread.interrupt(); 
+    }
+}
